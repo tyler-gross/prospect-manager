32d67780779c77a67469fbd92dcc69208b09b6c1
tyler-gross <tylergross0@yahoo.com>
2018-03-08 22:42:59 -0500

    First draft of prospect save form.

---
+++ src/ProspectManager/Controllers/ProspectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProspectManager.Controllers
+{
+    public class ProspectController : Controller
+    {
+        // GET: Prospect
+        public ActionResult Save()
+        {
+            return View();
+        }
+    }
+}
